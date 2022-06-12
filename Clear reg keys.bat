d5a71bca7658d5fca7f092bb562ebf2a591c2272
ref7 <stormtr56976211@gmail.com>
2022-06-12 10:18:56 +0530

    10:18:56.53 12-06-2022

---
+++ Clear reg keys.bat
@@ -0,0 +1,9 @@
+@echo off
+color b
+title Clear reg keys
+echo Clear reg keys
+pause
+reg delete HKEY_USERS\S-1-5-21-3038641696-3820312469-1113342023-1001_Classes\WOW6432Node\CLSID\ /f 
+cls 
+Done
+pause
---
+++ readme.txt
@@ -0,0 +1,1 @@
+just run this batch program as administrator
